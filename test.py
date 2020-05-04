44f44d083dd948263ac0484facb8773ff3304659
Alexandre <alexandre.Dareau@gmail.com>
2020-05-04 14:37:47 +0200

    add test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+Author   : alex
+Created  : 2020-05-04 14:36:08
+Modified : 2020-05-04 14:37:15
+
+Comments :
+'''
+from sequence import PulseSequence
+from shapes import PulseShape
+from numpy import pi
+
+rect_pulse = PulseShape(pulse_type='rect', time_offset=0)
+rect_pulse.laser_phase = pi/4
+
+seq = PulseSequence()
+seq.add_pulse(rect_pulse)
+seq.add_pulse(pulse_type='rect', time_offset=pi, pulse_duration=pi)
+U = seq.propagator()
+print(U)
+
+# -- plot ?
+rect_pulse.plot_pulse(time_norm=pi)
